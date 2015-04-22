72b2c74f35f2ea9c4524efcb484936244ad096fd
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2015-04-22 14:30:50 -0400

    python : rearranged files to match alpscore modules

---
+++ include/alps/utilities/python.hpp
@@ -0,0 +1,10 @@
+#ifndef ALPS_INCLUDE_PYTHON_u1asd37
+#define ALPS_INCLUDE_PYTHON_u1asd37
+
+#include <alps/utilities/boost_python.hpp>
+#include <alps/utilities/extract_from_pyobject.hpp>
+#include <alps/utilities/get_numpy_type.hpp>
+#include <alps/utilities/import_numpy.hpp>
+#include <alps/utilities/make_deepcopy.hpp> 
+
+#endif // #ifndef ALPS_INCLUDE_PYTHON_u1asd37
